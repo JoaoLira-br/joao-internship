df51a7b137614636e2466ace6f8b5ec7b49945f8
Joao Victor Lira <lira440440@gmail.com>
2024-11-02 17:08:51 -0300

    adds successfull hot collection request

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import { getNFTS } from "../../services/hot_collections";
+
 
 const HotCollections = () => {
+  const [nfts, setNFTs] = React.useState([]);
+  useEffect(() => {
+
+    const fetchNFTs = async () => {
+      const res =  await getNFTS();
+      console.log(`res`, res);
+      res?.status === 200 && setNFTs(res.data);
+    };
+
+    fetchNFTs(); // Call the async function
+
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,7 +30,30 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {nfts.length > 0 ? nfts.map((nft, index) => (
+
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+              <div className="nft_coll">
+                <div className="nft_wrap">
+                  <Link to="/item-details">
+                    <img src={nft.nftImage} className="lazy img-fluid" alt="" />
+                  </Link>
+                </div>
+                <div className="nft_coll_pp">
+                  <Link to="/author">
+                    <img className="lazy pp-coll" src={nft.authorImage} alt="" />
+                  </Link>
+                  <i className="fa fa-check"></i>
+                </div>
+                <div className="nft_coll_info">
+                  <Link to="/explore">
+                    <h4>{nft.title}</h4>
+                  </Link>
+                  <span>{nft.code}</span>
+                </div>
+              </div>
+            </div>
+          )) : new Array(4).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
